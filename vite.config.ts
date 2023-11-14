88ebce9c039b278048748a1a6a1eece3fface083
Jon Couch <joncouch4@hotmail..com>
2023-11-14 05:32:22 +0000

    added minified bundling

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 
+import terser from "@rollup/plugin-terser";
+
 export default defineConfig({
   build: {
     target: "es2022",
+    rollupOptions: {
+      plugins: [terser()],
+    },
   },
 });
